9eb461dd63376f31cf0da4048380c4361e3e0060
juliusappiah-dev <julius.crossmark@gmail.com>
2025-07-30 11:37:00 +0000

    Add files via upload

---
+++ config/session.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('session.gc_maxlifetime', 1800); // 30 minutes
+session_set_cookie_params(1800);
+
+session_start();
+
+// Regenerate session ID periodically
+if (!isset($_SESSION['last_regeneration'])) {
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+} elseif (time() - $_SESSION['last_regeneration'] > 300) { // Every 5 minutes
+    session_regenerate_id(true);
+    $_SESSION['last_regeneration'] = time();
+}
+?>
